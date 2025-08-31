e1e19c2886959b85e39e2aa5896c90605d9f3370
Hemit Patel <hemitvpatel@gmail.com>
2025-08-31 14:15:40 -0400

    change nav link name from study ai to ai study planner (moreaccurate)

--- src/data/common/navbar.ts
+++ src/data/common/navbar.ts
@@ -3,7 +3,7 @@ import { NavItem } from "@/types/common/navbar";
 export const menuItems: NavItem[] = [
   { label: "Dashboard", href: "/dashboard" },
   { label: "Practice", href: "/practice/math" },
-  { label: "Study AI", href: "/ai" },
+  { label: "AI Study Planner", href: "/ai" },
   { label: "Shop", href: "/shop" },
   { label: "Team", href: "/team" },
   { label: "Leaderboard", href: "/leaderboard" },
