74ac150c6b42815cf8c13cd8ac3b417f66f4de57
Hemit Patel <hemitvpatel@gmail.com>
2026-03-19 01:19:22 -0400

    fix: calculator feature

--- src/app/practice/[type]/page.tsx
+++ src/app/practice/[type]/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SubjectSidebar from "@/features/practice/components/SubjectSidebar";
 import QuestionContent from "@/features/practice/components/QuestionContent";
 import SessionProgress from "@/features/practice/components/SessionProgress";
+import { CalculatorOptions } from "@/features/practice/components/CalculatorOption";
 import {
   englishSubjectsArray,
   mathSubjectsArray,
@@ -12,10 +13,16 @@ import { Difficulty } from "@/features/practice/types/difficulty";
 import { EnglishSubjects, Type } from "@/features/practice/types/subject";
 import { useParams } from "next/navigation";
 import { QUESTION_IS_CORRECT_POINTS } from "@/shared/data/constant";
+import { useCalculatorModalStore } from "@/stores/modals";
+import { useCalcModeModalStore } from "@/stores/calc";
 
 const PracticePage = () => {
   const params = useParams();
   const { type } = params;
+  const closeCalculatorModal = useCalculatorModalStore(
+    (state) => state.closeModal,
+  );
+  const setCalcMode = useCalcModeModalStore((state) => state.setMode);
 
   if (type !== "math" && type !== "english") {
     throw new Error("Proper slug is not provided. Try again.");
@@ -34,6 +41,13 @@ const PracticePage = () => {
     throw new Error("Type query parameter is required");
   }
 
+  useEffect(() => {
+    if (type !== "math") {
+      closeCalculatorModal();
+      setCalcMode("none");
+    }
+  }, [type, closeCalculatorModal, setCalcMode]);
+
   const handleAnswered = (isCorrect: boolean, coinReward?: number) => {
     if (isCorrect) {
       const reward = coinReward ?? QUESTION_IS_CORRECT_POINTS;
@@ -50,42 +64,47 @@ const PracticePage = () => {
     }
   };
   return (
-    <div className="flex flex-col md:flex-row min-h-screen gap-6 px-4 md:px-10 py-6">
-      {/* Sidebar */}
-      <aside className="w-full md:w-72 rounded-md p-4 overflow-y-auto">
-        <SubjectSidebar
-          subject={type === "math" ? "Math" : "English"}
-          selectedTopic={selectedTopic}
-          setSelectedTopic={setSelectedTopic}
-          subjects={type === "math" ? mathSubjectsArray : englishSubjectsArray}
-          difficulty={difficulty}
-          setDifficulty={setDifficulty}
-        />
-      </aside>
-
-      {/* Question Content */}
-      <main className="flex-1 flex flex-col space-y-8">
-        <div className="rounded-md p-6 flex-grow overflow-auto">
-          <QuestionContent
-            subject={selectedTopic}
+    <>
+      <div className="flex flex-col md:flex-row min-h-screen gap-6 px-4 md:px-10 py-6">
+        {/* Sidebar */}
+        <aside className="w-full md:w-72 rounded-md p-4 overflow-y-auto">
+          <SubjectSidebar
+            subject={type === "math" ? "Math" : "English"}
+            selectedTopic={selectedTopic}
+            setSelectedTopic={setSelectedTopic}
+            subjects={
+              type === "math" ? mathSubjectsArray : englishSubjectsArray
+            }
             difficulty={difficulty}
-            type={type as Type}
-            onAnswered={handleAnswered}
+            setDifficulty={setDifficulty}
           />
-        </div>
-      </main>
+        </aside>
 
-      {/* Score Panel */}
-      <aside className="w-full md:w-72 rounded-md p-4 overflow-y-auto">
-        <SessionProgress
-          correctCount={correctCount}
-          wrongCount={wrongCount}
-          currentStreak={currentStreak}
-          maxStreak={maxStreak}
-          coinsEarned={coinsEarned}
-        />
-      </aside>
-    </div>
+        {/* Question Content */}
+        <main className="flex-1 flex flex-col space-y-8">
+          <div className="rounded-md p-6 flex-grow overflow-auto">
+            <QuestionContent
+              subject={selectedTopic}
+              difficulty={difficulty}
+              type={type as Type}
+              onAnswered={handleAnswered}
+            />
+          </div>
+        </main>
+
+        {/* Score Panel */}
+        <aside className="w-full md:w-72 rounded-md p-4 overflow-y-auto">
+          <SessionProgress
+            correctCount={correctCount}
+            wrongCount={wrongCount}
+            currentStreak={currentStreak}
+            maxStreak={maxStreak}
+            coinsEarned={coinsEarned}
+          />
+        </aside>
+      </div>
+      {type === "math" && <CalculatorOptions />}
+    </>
   );
 };
 
