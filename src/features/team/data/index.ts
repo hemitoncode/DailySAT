42d76b1493c100635388550bc272ff6dc91e310a
hemit patel 🧑🏽‍💻 <hemitvpatel@gmail.com>
2026-03-19 20:57:23 -0400

    feat: remove other ppl

--- src/features/team/data/index.ts
+++ src/features/team/data/index.ts
@@ -15,28 +15,5 @@ export const teamMember = [
     src: "/people/hemit.png",
     quote:
       "Hi! I'm Hemit! A passionate high school student interested in Computer Science. Specifically, I am interested in AI, which I am currently learning, but I'm also well-versed in web-development.. ",
-  },
-  {
-    name: "Wilman Chan",
-    linkedIn: "https://www.linkedin.com/in/wilman-chan-03a468286/",
-    designation: "Chief Marketing Officer",
-    src: "/people/G10_Yearbook_Picture.png",
-    quote: "Hi there, I am Wilman! I am a student founder passionate about social media marketing & content creation."
-  },
-  {
-    name: "Gautham Korrapati",
-    linkedIn: "https://www.linkedin.com/in/gautham-korrapati-946369272/",
-    designation: "Chief Technology Officer",
-    src: "/people/gautham.png",
-    quote:
-      "Hey, I'm Gautham, a current student at COC and I just want to have fun.",
-  },
-  {
-    name: "Devesh Khilnani",
-    linkedIn: "https://www.linkedin.com/in/dkhilnani/",
-    designation: "Chief Business Officer",
-    src: "/people/devesh.png",
-    quote:
-      "I am a 17 year old high school and college student with a passion for R&D, business, & medicine. I love putting my energy and using my experience to every project, as I consistently seek new challenges.",
-  },
+  }
 ];
